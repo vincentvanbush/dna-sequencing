61a6620f8bdf963ffd2164268741879b64b60435
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-04-27 21:43:54 +0200

    Finds starting nuc for sequence

--- initial_heur.py
+++ initial_heur.py
@@ -15,9 +15,30 @@ def create_initial_solution(instance):
 		for oligo2 in oligos:
 			if oligo1 != oligo2:
 				overlaps_pairs[(oligo1, oligo2)] = oligo1.overlap(oligo2)
-	for o1, o2 in overlaps_pairs:
-		print o1, o2
-		print overlaps_pairs[(o1, o2)]
+	
+	# for oligo1, oligo2 in overlaps_pairs:
+	# 	print oligo1, oligo2
+	# 	print overlaps_pairs[(oligo1, oligo2)]
+
+	in_edges_for_nucs = {}
+	for oligo in oligos:
+		in_edges_for_nucs[oligo] = 0
+	for oligo1, oligo2 in overlaps_pairs:
+		in_edges_for_nucs[oligo2] += overlaps_pairs[(oligo1, oligo2)]
+	
+	oligo_with_min_in_edges = oligos[0]
+	min_in_edges_val = in_edges_for_nucs[oligo_with_min_in_edges]
+	
+	for oligo in in_edges_for_nucs:
+		if in_edges_for_nucs[oligo] < min_in_edges_val:
+			min_in_edges_val = in_edges_for_nucs[oligo]
+			oligo_with_min_in_edges = oligo
+
+	# print oligo_with_min_in_edges.nuc
+	# print min_in_edges_val
+
+
+	
 		
 
 
