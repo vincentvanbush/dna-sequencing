68d07a86fdc4342cc492b9c901972e9e49de0207
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-04-27 23:51:56 +0000

    added oligo_length property to Instance

--- solver.py
+++ solver.py
@@ -2,7 +2,11 @@ class Solution:
 
     def __init__(self):
         self.sequence = ''
-        self.overlaps = [] # (oligo1, oligo2, overlap_len)
+        self.overlaps = [] # (oligo1, oligo2, overlap_len, o1_pos)
+
+    def __init__(self, solution):
+        self.sequence = solution.sequence
+        self.overlaps = solution.overlaps
 
     def used_oligos_count(self):
         return len(self.overlaps) + 1
@@ -13,6 +17,10 @@ class Instance:
         self.oligos = oligos
         self.result_length = result_length
         self.solution = None
+
+    @property
+    def oligo_length(self):
+        return len(self.oligos[1].nuc)
 
     def solve(self, pipeline):
         for proc in pipeline:
