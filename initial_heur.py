78ae94252aad9209f8656d1665f097cbdf9e5bd1
Mateusz Moskwa <mateuszmos18@gmail.com>
2015-04-27 22:01:44 +0200

    Changes representation of oligos pairs

--- initial_heur.py
+++ initial_heur.py
@@ -12,19 +12,23 @@ def create_initial_solution(instance):
 	overlaps_pairs = {}
 	
 	for oligo1 in oligos:
+		overlaps_pairs[oligo1] = {}
 		for oligo2 in oligos:
 			if oligo1 != oligo2:
-				overlaps_pairs[(oligo1, oligo2)] = oligo1.overlap(oligo2)
+				overlaps_pairs[oligo1][oligo2] = oligo1.overlap(oligo2)
 	
-	# for oligo1, oligo2 in overlaps_pairs:
-	# 	print oligo1, oligo2
-	# 	print overlaps_pairs[(oligo1, oligo2)]
+	# for oligo1 in overlaps_pairs:
+	# 	for oligo2 in overlaps_pairs[oligo1]:
+	# 		print oligo1, oligo2
+	# 		print overlaps_pairs[oligo1][oligo2]
 
 	in_edges_for_nucs = {}
 	for oligo in oligos:
 		in_edges_for_nucs[oligo] = 0
-	for oligo1, oligo2 in overlaps_pairs:
-		in_edges_for_nucs[oligo2] += overlaps_pairs[(oligo1, oligo2)]
+	
+	for oligo1 in overlaps_pairs:
+		for oligo2 in overlaps_pairs[oligo1]:
+			in_edges_for_nucs[oligo2] += overlaps_pairs[oligo1][oligo2]
 	
 	oligo_with_min_in_edges = oligos[0]
 	min_in_edges_val = in_edges_for_nucs[oligo_with_min_in_edges]
@@ -36,6 +40,8 @@ def create_initial_solution(instance):
 
 	# print oligo_with_min_in_edges.nuc
 	# print min_in_edges_val
+
+
 
 
 	
